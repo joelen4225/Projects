d93094fc718a168797bff448599bffb285353cd8
joelen4225 <64109094+joelen4225@users.noreply.github.com>
2020-05-07 14:31:59 -0400

    Add files via upload

---
+++ SoundReactRainbow.ino
@@ -0,0 +1,60 @@
+#include <FastLED.h>
+
+// How many leds in your strip?
+#define NUM_LEDS 60
+
+#define DATA_PIN 2
+#define CLOCK_PIN 13
+
+int gHue = 0;
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+void setup() 
+{ 
+    FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);  // GRB ordering is assumed
+}
+
+void loop() 
+{ 
+  if (digitalRead(7) == 0)
+  {
+    CRGBPalette16 palette = PartyColors_p;
+      FastLED.setBrightness(25);
+      BlackLED();
+      FastLED.show();
+  }
+  
+  if (digitalRead(7) == 1)
+  {
+    FastLED.setBrightness(225);
+    RainbowWave();
+    FastLED.show();
+  }
+}
+
+
+void BlackLED()
+{
+  int BeatsPerMinute = 62;
+  CRGBPalette16 palette = CloudColors_p;
+ 
+  for( int i = 0; i < NUM_LEDS; i++) 
+  { 
+    leds[i] = CHSV(0, 0, 0);
+  }
+}
+
+void RainbowWave()
+{
+  // Rainbow Strip Pulsing at a Defined Beats-Per-Minute (BPM)
+  int BeatsPerMinute = 62;
+  CRGBPalette16 palette = RainbowStripeColors_p;
+  int beat = beatsin8( BeatsPerMinute, 64, 255);
+  
+  for( int i = 0; i < NUM_LEDS; i++) 
+  { 
+    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
+  }
+}
